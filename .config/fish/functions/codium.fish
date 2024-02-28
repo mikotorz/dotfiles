8fbae56b5c83fd56e86414419bd761712ddcf9c5
mikotorz <95578784+mikotorz@users.noreply.github.com>
2024-02-28 19:36:06 +0900

    Delete .config directory

--- .config/fish/functions/codium.fish
+++
@@ -1,4 +0,0 @@
-function codium --wraps='/usr/share/codium-insiders/codium-insiders --ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --enable-wayland-ime' --description 'alias codium=/usr/share/codium-insiders/codium-insiders --ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --enable-wayland-ime'
-  /usr/share/codium-insiders/codium-insiders --ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --enable-wayland-ime $argv
-        
-end
